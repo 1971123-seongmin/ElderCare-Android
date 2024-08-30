ea0a916dfa1f0b6911fe73de83e7b7404a2057e0
Seongmin <arcn1345@gmail.com>
2024-08-30 17:52:06 +0900

    feat: HubHomeFragment 블루투스용 임시버튼 추가

--- app/src/main/java/com/example/elder_care/ui/install_guide/HubHomeFragment.kt
+++ app/src/main/java/com/example/elder_care/ui/install_guide/HubHomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.elder_care.ui.install_guide
 
+import android.util.Log
 import androidx.core.view.MenuHost
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -22,34 +23,35 @@ class HubHomeFragment : BaseFragment<FragmentHubHomeBinding>(R.layout.fragment_h
 
     private fun initSetting() {
         setButton()
+        setBlueTooth()
     }
 
     private fun setButton() {
         binding.fragmentHomeGuideBtn.setOnClickListener {
-            val action = HubHomeFragmentDirections.actionHomeFragmentToHubInfoFragment()
+            val action = HubHomeFragmentDirections.actionHubHomeFragmentToHubInfoFragment()
             findNavController().navigateSafe(action.actionId)
         }
     }
 
-//    private fun setBlueTooth() {
-//
-//        bluetoothManager = BluetoothManager(requireContext())
-//        bluetoothManager.setViewModel(bluetoothDataViewModel)
-//
-//        if (bluetoothManager.isBluetoothSupport()) {
-//
-//            // 블루투스 OFF 이면 ON -> 페어링 가능한 블루투스 목록만 가져옴 (페어링 된 블루투스 목록은 못가져옴)
-//            binding.homeFindBluetoothBtn.setOnClickListener {
-//                bluetoothManager.findDevice()
-//            }
-//
-//            // 블루투스 페어링 목록 검색 (블루투스 ON)
-//            binding.homeGetBluetoothBtn.setOnClickListener {
-//                bluetoothManager.getPairedDevices()
-//            }
-//            val data = bluetoothDataViewModel.getConnectedDeviceData()
-//            Log.d("로그", "센서 값 : $data")
-//        }
-//    }
+    private fun setBlueTooth() {
+
+        bluetoothManager = BluetoothManager(requireContext())
+        bluetoothManager.setViewModel(bluetoothDataViewModel)
+
+        if (bluetoothManager.isBluetoothSupport()) {
+
+            // 블루투스 OFF 이면 ON -> 페어링 가능한 블루투스 목록만 가져옴 (페어링 된 블루투스 목록은 못가져옴)
+            binding.bluetoothBtn.setOnClickListener {
+                bluetoothManager.findDevice()
+            }
+
+            // 블루투스 페어링 목록 검색 (블루투스 ON)
+            binding.bluetoothGetBtn.setOnClickListener {
+                bluetoothManager.getPairedDevices()
+            }
+            val data = bluetoothDataViewModel.getConnectedDeviceData()
+            Log.d("로그", "센서 값 : $data")
+        }
+    }
 
 }
